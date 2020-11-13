0ff36adc4b6a05b632bad32e06cc740644a368f4
ahanafy10 <ahanafy787@gmail.com>
2020-11-13 14:17:46 -0600

    finshed

--- script.js
+++ script.js
@@ -1,15 +1,100 @@
+<script>
+var bigpic = document.getElementById("gal");
+var total = document.getElementsByTagName("img");
+var gallery = document.getElementById('gallery');
+var nature = document.getElementById('nature');
+var lake = document.getElementById('lake');
+var mountain = document.getElementById('mountain');
+var sun = document.getElementById('sun');
+var beach = document.getElementById('beach');
+var view = document.getElementById('view');
+var water = document.getElementById('water');
+var clouds = document.getElementById('clouds');
+var cool = document.getElementById('cool');
+var pi = document.getElementById('pi');
+var pnw = document.getElementById('pnw');
+var house = document.getElementById('house');
+var arr = [];
+var index = 0;
 
-function zoom(){
-  document.getElementById("photos").style.height="1000px";
-  document.getElementById("photos").style.width="1000px";
+
+for(i = 0;i<total.length;i++){
+  arr.push(String(total[i].src));
 }
-function hover(){
-document.getElementById("photos").style.filter="brightness(80%)";
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  gallery.style.display = "none";
 }
-function undo(){
-  document.getElementById("photos").style.filter="brightness(100%)";
+
+nature.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
 }
-function zoomout(){
-  document.getElementById("photos").style.height="300px";
-  document.getElementById("photos").style.width="300px";
+lake.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
 }
+mountain.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+sun.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+beach.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+view.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+water.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+clouds.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+cool.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+pi.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+pnw.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+house.onclick = function(){
+  index = arr.indexOf(String(this.src));
+  gallery.style.display = "block";
+  bigpic.src = this.src;
+}
+
+function next(){
+index +=1;
+bigpic.src = arr[index];
+}
+
+function prev(){
+index -=1;
+bigpic.src = arr[index];
+}
+
+</script>
